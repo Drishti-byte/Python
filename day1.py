035dc297df5f82346cb9ae665c8785ae837c4ebe
Drishti-byte <drishtibansal06@gmail.com>
2025-08-14 13:32:04 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,5 @@
+#program to print your introduction 
+print("-------------------------------------")
+print("My name is Drishti Bansal")
+print("I am currently living in Delhi")
+print("-------------------------------------")
