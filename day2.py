284b55df3a25e6f5075dfaee703e01988338bd3d
Drishti-byte <drishtibansal06@gmail.com>
2025-08-15 13:58:46 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,8 @@
+#program to input the radius of a circle and calculate the area and circumference
+r = float(input("Enter the radius of the circle(in cm): "))
+
+area = 3.14 * r * r 
+print("Area of circle:",area, "cm sq")
+
+peri = 2 * 3.14 * r 
+print("Perimeter of circle:",peri, "cm")
