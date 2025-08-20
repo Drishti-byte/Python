47b2c58a35b9c9fb8041e6675d2006e64a785a5f
Drishti-byte <drishtibansal06@gmail.com>
2025-08-20 17:58:56 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,11 @@
+#program to check whether the year is a leap year or not 
+year = int(input("Enter the year:"))
+if year % 100 == 0:
+    if year % 400 == 0:
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif year % 4 == 0:
+    print("It is a leap year") 
+else:
+    print("It is not a leap year")
