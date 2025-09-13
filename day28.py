df691ef6dd69c8a1cba6a896a6aa2d5b5d56521a
Drishti-byte <drishtibansal06@gmail.com>
2025-09-13 10:00:46 +0530

    Add files via upload

---
+++ day28.py
@@ -0,0 +1,8 @@
+#program to print half pyramid 
+n = int(input("Enter the number of rows:"))
+for i in range(1,n+1):
+    for j in range(n - i):
+        print(" ",end = '') 
+    for k in range(2 * i - 1):
+        print("*",end = '')
+    print()
