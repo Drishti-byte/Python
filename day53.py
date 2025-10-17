0af8a911235658d9087b6f3427f985394f49432a
Drishti Bansal <drishtibansal06@gmail.com>
2025-10-17 18:06:56 +0530

    Add files via upload

---
+++ day53.py
@@ -0,0 +1,10 @@
+#program to count the frequency of each letter in a string 
+txt = input("Enter a string:") 
+d = {} 
+for ch in txt:
+    if ch not in d:
+        d[ch] = 1 
+    else: 
+        d[ch] += 1
+for k in d:
+    print(k, "occurs", d[k], "times")
