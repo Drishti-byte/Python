41535f14fb90abdc438b4676bac73035cb3b1469
Drishti Bansal <drishtibansal06@gmail.com>
2025-11-01 10:14:55 +0530

    Add files via upload

---
+++ day67.py
@@ -0,0 +1,17 @@
+#program to check if a word is easy to pronounce 
+word = input("Enter a word:") 
+c = 0
+for ch in word:
+    if ch.isalpha():
+        if ch not in "aeiouAEIOU":
+            c += 1 
+            if c == 4:
+                print("It is difficult to pronounce")
+                break 
+        else:
+            c = 0
+    else:
+        print("Invalid Input")
+        break 
+else: 
+    print("It is easy to pronounce") 
