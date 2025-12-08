4a1eb071d6b0beb1b8cfda24c5aeb6c4a29165e5
Drishti Bansal <drishtibansal06@gmail.com>
2025-12-08 11:00:30 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,5 @@
+#program to input 2 floating numbers and print their average
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+average = (a + b)/ 2
+print("The average is",average)
