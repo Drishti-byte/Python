ca46b76df30e8a4bcd5d5763b74799d8f2af23ff
Drishti Bansal <drishtibansal06@gmail.com>
2025-12-09 10:54:38 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#program to perform all the operations on two numbers 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+res = num1 + num2 
+print("Sum is",res)
+res = num1 - num2 
+print("Difference is",res)
+res = num1 * num2 
+print("Product is",res)
+res = num1 / num2 
+print("Division is",res)
+res = num1 // num2 
+print("Quotient is",res)
+res = num1 % num2 
+print("Remainder is",res)
