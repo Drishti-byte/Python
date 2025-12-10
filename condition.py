bf5c1787a22d74356eb92794e6a6fef612bd2886
Drishti Bansal <drishtibansal06@gmail.com>
2025-12-10 19:32:51 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,6 @@
+#program to check whether we can vote and apply for license or not 
+age = int(input("Enter your age:"))
+if(age >= 18):
+    print("You can vote and apply for license as well")
+else:
+    print("You cannot vote and cannot apply for license")
