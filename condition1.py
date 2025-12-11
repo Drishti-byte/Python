20bcf1e422887d47b574caa4047641de9aa1cb2b
Drishti Bansal <drishtibansal06@gmail.com>
2025-12-11 11:15:18 +0530

    Add files via upload

---
+++ condition1.py
@@ -0,0 +1,10 @@
+#program to allot the grades based on marks 
+marks = int(input("Enter your marks:"))
+if(marks >= 90):
+    print("Your grade is A")
+elif(marks >= 80 and marks < 90):
+    print("Your grade is B")
+elif(marks >= 70 and marks < 80):
+    print("Your grade is C")
+else:
+    print("Your grade is D")
