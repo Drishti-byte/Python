b586a9cdd51685f8b57e285476081be75c82b626
Drishti Bansal <drishtibansal06@gmail.com>
2025-12-23 19:37:59 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,10 @@
+#program to read from a file 
+# f = open("demo.txt", "r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()  
+f = open("mystory.txt", "r")
+data = f.read()
+print(data)
+f.close()
