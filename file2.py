c5e87b23e243bcc9f18f8c3cd22a15a38aba6a6a
Drishti Bansal <drishtibansal06@gmail.com>
2025-12-30 15:12:33 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,9 @@
+#program to write into a file 
+# f = open("demo1.txt","w")
+# f.write("I will learn JavaScript tomorrow.")
+# f.close() 
+f = open("mystory.txt", "w")
+f.write("This is a sample file")
+f.write("\nIt is created using Python Program")
+f.write("\nPython is fun")
+f.close() 
