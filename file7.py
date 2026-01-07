9d820426aba5cd580ed998654f35ff7216e571ca
Drishti Bansal <drishtibansal06@gmail.com>
2026-01-07 11:02:01 +0530

    Add files via upload

---
+++ file7.py
@@ -0,0 +1,8 @@
+#program to search a word in the file 
+word = "learning"
+with open("practice.txt", "r") as f:
+    data = f.read()
+    if(data.find(word) != -1):
+        print("Found")
+    else:
+        print("Not Found")
