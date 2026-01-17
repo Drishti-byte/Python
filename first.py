a9033d67ce7fbf2c757422166a3e2aaadad30be9
Drishti Bansal <drishtibansal06@gmail.com>
2026-01-17 12:17:31 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,22 @@
+print("Hello World")
+print("Drishti Bansal")
+print("Hello World", "My name is Drishti Bansal")
+name = "Drishti"
+age = 18
+price = 25.99
+print(name)
+print(age)
+print(price)
+print("My name is: ",name)
+print("My age is: ",age)
+print(type(name))
+print(type(age))
+print(type(price))
+old = False 
+a = None 
+print(type(old))
+print(type(a))
+a = 90
+print(id(a))
+d = 90 
+print(id(d))
