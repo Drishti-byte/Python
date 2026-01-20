d1d6074a93a448e4eea439ba93d5d672bea45014
Drishti Bansal <drishtibansal06@gmail.com>
2026-01-20 22:35:47 +0530

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,5 @@
+#program to print the length of the list using functions 
+cities = {"Delhi", "Mumbai", "Indore", "Gujarat", "Noida", "Gurgaon"}
+def len_list(list):
+    print(len(list))
+len_list(cities)
