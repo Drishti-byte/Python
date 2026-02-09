11ed5f3ea59ac8ac4ae7cbb63f6ee7a1393ff169
Drishti Bansal <drishtibansal06@gmail.com>
2026-02-09 13:57:01 +0530

    Add files via upload

---
+++ lists1.py
@@ -0,0 +1,9 @@
+#program to input movie names from the user and store them in a list
+movies = []
+m1 = input("Enter your 1st favourite movie:")
+m2 = input("Enter your 2nd favourite movie:")
+m3 = input("Enter your 3rd favorite movie:")
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
+print(movies)
